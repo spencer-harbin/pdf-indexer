4519b8ae0f1fc59b24f50dfce2a8e94e53c7323e
spencer-harbin <sdh7301@gmail.com>
2024-09-03 20:23:58 -0400

    tentative first draft

--- pdf_indexer/src/main.rs
+++ pdf_indexer/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufWriter;
+use pdf_extract::extract_text;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pdf_path = "example/path/for/now.pdf";
+
+    let text = extract_text(pdf_path)?;
+
+    println!("Extracted text:\n{}", text);
+
+    // save extracted text to txt file
+    let output_path = "output.txt";
+    let output_file = File::create(output_path)?;
+    let mut writer = BufWriter::new(output_file);
+    std::io::Write::write_all(&mut writer, text.as_bytes())?;
+
+    Ok(())
 }
