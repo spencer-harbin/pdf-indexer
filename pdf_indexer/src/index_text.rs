2aae3f00581ccd4686a3631ef0f1973ac2a8e376
spencer-harbin <sdh7301@gmail.com>
2024-09-10 23:05:57 -0400

    update to index mod

--- pdf_indexer/src/index_text.rs
+++ pdf_indexer/src/index_text.rs
@@ -1,5 +1,10 @@
-pub struct InvIndex {
+use std::collections::HashMap; 
 
+type DocumentID = String; 
+type Position = usize; 
+
+pub struct InvIndex {
+    index: HashMap<String, HashMap<DocumentID, Vec<Position>>> // word -> doc ID -> positions in document
 }
 
 impl InvIndex {
