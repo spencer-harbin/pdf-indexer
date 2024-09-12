d8c1469f75b42d4ebbc09ab76dca2b7f4b730fdb
spencer-harbin <sdh7301@gmail.com>
2024-09-11 21:45:46 -0400

    add three functions to index_text

--- pdf_indexer/src/index_text.rs
+++ pdf_indexer/src/index_text.rs
@@ -8,5 +8,30 @@ pub struct InvIndex {
 }
 
 impl InvIndex {
+    pub fn new() -> Self {
+        InvIndex{
+            index: HashMap::new() 
+        }
+    }
 
+    pub fn index_document(&mut self, doc_id: DocumentID, text: &str) {
+        for (position, word) in text.split_whitespace().enumerate() {
+            let word = word.to_lowercase(); 
+
+            let entry = self.index.entry(word).or_insert_with(HashMap::new); 
+            let doc_entry = entry.entry(doc_id.clone()).or_insert_with(Vec::new); 
+            doc_entry.push(position); 
+
+        }
+    }
+
+    // delete later
+    pub fn print_index(&self) {
+        for (word, docs) in &self.index {
+            println!("Word: '{}", word); 
+            for (doc_id, positions) in docs {
+                println!("Document: {}, Positions: {:?}", doc_id, positions)
+            }
+        }
+    }
 }
