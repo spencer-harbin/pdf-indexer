c381806b579b81e36549ea574cc414f0f52e74f0
spencer-harbin <sdh7301@gmail.com>
2024-09-12 22:20:56 -0400

    test index function

--- pdf_indexer/src/main.rs
+++ pdf_indexer/src/main.rs
@@ -1,9 +1,14 @@
 mod extract_pdf_text;
 mod search_text;
+mod index_text;
 
-use std::io; 
+use std::io;
+
+use index_text::InvIndex; 
 
 fn main() -> io::Result<()> {
+    let mut index = InvIndex::new(); 
+
     let pdf_path = "/Users/spencer/Documents/email_papers_script/sample_pdfs/test.pdf"; 
     let python_script_path = "/Users/spencer/Documents/pdf_index/pdf_indexer/python/extract_pdf_text.py"; 
 
@@ -11,6 +16,11 @@ fn main() -> io::Result<()> {
 
     // process rest of stuff here 
     println!("Extracted text:\n{}", extracted_text); 
+
+    // index the test doc
+    index.index_document("doc1.txt".to_string(), &extracted_text); 
+
+    index.print_index();
 
     Ok(())
 }
